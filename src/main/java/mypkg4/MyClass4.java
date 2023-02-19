db91ffaf3f87acd2a07d3894aceb0c4531d66ff7
[Priyanshi Tyagi] <priyanshityagi@Priyanshis-MacBook-Air.local>
2023-02-19 12:23:37 +0530

    first commit

---
+++ src/main/java/mypkg4/MyClass4.java
@@ -0,0 +1,20 @@
+package mypkg4;
+
+
+public class MyClass4 {
+	
+	public void show()
+	{
+		System.out.println("Hello , Welcome To Jenkins");
+	}
+	
+
+public static void main (String[] args)
+{
+	MyClass4 ob  = new MyClass4();
+	ob.show();
+}
+
+
+}
+
